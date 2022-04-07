234036ad1c4e3743901ba4201705463064b3f2ff
Mikayla Labadie <laba0031@algonquinlive.com>
2022-04-07 14:45:20 -0400

    starts html, css and js

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+//!TOGGLE NAVIGATION MENU AND ICONS
+const menuToggleButton = document.querySelector('.menu-toggle-button')
+const menuElement = document.querySelector('.menu')
+
+const toggleMenu = () => {
+    menuElement.classList.toggle('active');
+};
+menuToggleButton.addEventListener('click', toggleMenu);
+// !REMOVE ACTIVE CLASS FROM AND ICON ON LINK CLICK
+
+// !TOGGLE THEME AND STORE SELECTION WITHIN LOCAL STORAGE
+
+// !SCROLL REVEAL
